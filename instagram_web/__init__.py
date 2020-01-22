5efdb6c0ab56455a419f781799cc8ef736551271
Aldino Dwi Anggawan <aldinoanggawan@gmail.com>
2020-01-22 17:27:53 +0800

    Added custom error page 404

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -15,6 +15,9 @@ app.register_blueprint(session_blueprint, url_prefix="/session")
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route("/")
 def home():
